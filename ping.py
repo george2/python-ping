b9fc3acb2c36ccc895d1f7ba7336b951dc033ce9
Francesco Frassinelli <fraph24@gmail.com>
2012-08-09 18:58:22 +0200

    Faster and cleaner checksum creation

--- ping.py
+++ ping.py
@@ -16,6 +16,7 @@
 """
 
 
+import array
 import os
 import select
 import signal
@@ -48,35 +49,19 @@ def calculate_checksum(source_string):
     packed), but this works.
     Network data is big-endian, hosts are typically little-endian
     """
-    countTo = (int(len(source_string) / 2)) * 2
-    sum = 0
-    count = 0
+    if len(source_string)%2:
+        source_string += "\x00"
+    converted = array.array("H", source_string)
+    if sys.byteorder == "big":
+        converted.byteswap()
+    val = sum(converted)
 
-    # Handle bytes in pairs (decoding as short ints)
-    loByte = 0
-    hiByte = 0
-    while count < countTo:
-        if (sys.byteorder == "little"):
-            loByte = source_string[count]
-            hiByte = source_string[count + 1]
-        else:
-            loByte = source_string[count + 1]
-            hiByte = source_string[count]
-        sum = sum + (ord(hiByte) * 256 + ord(loByte))
-        count += 2
-
-    # Handle last byte if applicable (odd-number of bytes)
-    # Endianness should be irrelevant in this case
-    if countTo < len(source_string): # Check for odd length
-        loByte = source_string[len(source_string) - 1]
-        sum += ord(loByte)
-
-    sum &= 0xffffffff # Truncate sum to 32 bits (a variance from ping.c, which
+    val &= 0xffffffff # Truncate val to 32 bits (a variance from ping.c, which
                       # uses signed ints, but overflow is unlikely in ping)
 
-    sum = (sum >> 16) + (sum & 0xffff)    # Add high 16 bits to low 16 bits
-    sum += (sum >> 16)                    # Add carry from above (if any)
-    answer = ~sum & 0xffff                # Invert and truncate to 16 bits
+    val = (val >> 16) + (val & 0xffff)    # Add high 16 bits to low 16 bits
+    val += (val >> 16)                    # Add carry from above (if any)
+    answer = ~val & 0xffff                # Invert and truncate to 16 bits
     answer = socket.htons(answer)
 
     return answer
